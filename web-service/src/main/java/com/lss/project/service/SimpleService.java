185f7e73acf3581ec3e696d640666ff333c1fe1d
lssemail <lssemail@126.com>
2017-10-12 12:02:42 +0800

    init project

---
+++ web-service/src/main/java/com/lss/project/service/SimpleService.java
@@ -0,0 +1,12 @@
+package com.lss.project.service;
+
+/**
+ * Created by lssemail on 2017/10/12.
+ */
+public class SimpleService {
+
+    public static String ServiceDesc(){
+
+        return "service desc";
+    }
+}
